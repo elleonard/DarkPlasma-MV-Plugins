10bdd29e7a3a831295da6b837d14ba91c5e07a60
plasma <librarian256@gmail.com>
2021-09-11 18:54:36 +0900

    1.0.1 Game_Mapを上書きするようなプラグインとの競合を修正

--- src/codes/CustomEncounterList/DarkPlasma_CustomEncounterList.js
+++ src/codes/CustomEncounterList/DarkPlasma_CustomEncounterList.js
@@ -11,20 +11,23 @@ DataManager.extractMetadata = function (data) {
   }
 };
 
-Game_Map = class extends Game_Map {
-  encounterList() {
+function Game_Map_CustomEncounterListMixIn(gameMap) {
+  const _encounterList = gameMap.encounterList;
+  gameMap.encounterList = function() {
     const result = this.enabledCustomEncounterList();
-    return result ? result : super.encounterList();
+    return result ? result : _encounterList.call(this);
   }
 
   /**
    * 有効なエンカウント一覧を取得する
    * @return {RPG.Map.Encounter[]|null}
    */
-  enabledCustomEncounterList() {
+  gameMap.enabledCustomEncounterList = function() {
     const setting = settings.encounters
       .find(encounterSetting => $dataMap.customEncounterList.includes(encounterSetting.id)
         && $gameSwitches.value(encounterSetting.conditionSwitch));
     return setting ? setting.list : null;
   }
-}
+};
+
+Game_Map_CustomEncounterListMixIn(Game_Map.prototype);
