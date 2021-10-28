ef9d37a957821d75f86dffa932edd67e514c93bf
plasma <librarian256@gmail.com>
2021-10-28 13:12:18 +0900

    2.0.0 自動ハイライトプラグイン

---
+++ src/codes/AutoHighlight/DarkPlasma_AutoHighlight.js
@@ -0,0 +1,161 @@
+import { settings } from './_build/DarkPlasma_AutoHighlight_parameters';
+
+class HighlightWords {
+  constructor() {
+    this._highlightWords = [];
+    this._sortedWords = null;
+    this._colors = null;
+  }
+
+  /**
+   * @return {RegExp}
+   */
+  getRegExp() {
+    return new RegExp(
+      `(${this.sortedWords().join("|")})`,
+      "gi"
+    );
+  }
+
+  /**
+   * @return {RegExp}
+   */
+  getRegExpForRuby() {
+    return new RegExp(
+      `(^${this.sortedWords().join("$|")})`,
+      "i"
+    );
+  }
+
+  /**
+   * 長さ順にソートしたハイライト語句一覧
+   * @return {string[]}
+   */
+  sortedWords() {
+    if (!this._sortedWords || this._needsRefreshCache) {
+      this.refreshCache();
+    }
+    return this._sortedWords;
+  }
+
+  /**
+   * @param {HighlightWord} highlightWord ハイライトする語句と色
+   */
+  add(highlightWord) {
+    this._highlightWords.push(highlightWord);
+    this._needsRefreshCache = true;
+  }
+
+  refreshCache() {
+    /**
+     * 毎度ソートするのはパフォーマンス的に許容できないため、キャッシュする
+     */
+    this._sortedWords = this._highlightWords.map(word => word.word).sort((a, b) => b.length - a.length);
+    this._colors = {};
+    /**
+     * パフォーマンスに気を使い、ランダムアクセスできるようにキャッシュする
+     */
+    this._highlightWords.forEach(highlightWord => {
+      this._colors[highlightWord.word] = highlightWord.color;
+    })
+    this._needsRefreshCache = false;
+  }
+
+  /**
+   * ハイライト色を返す
+   * @param {string} word ハイライトする語句
+   * @return {string|number}
+   */
+  findColorByWord(word) {
+    if (!this._colors) {
+      this.refreshCache();
+    }
+    return this._colors[word].startsWith("#") ? this._colors[word] : Number(this._colors[word]);
+  }
+
+  /**
+   * テキスト内の指定語句をハイライトして返す
+   * @param {string} text ハイライト対象テキスト
+   * @return {string}
+   */
+  highlightText(text) {
+    return text.replace(this.getRegExp(), match => {
+      return `\x1bC[${this.findColorByWord(match)}]${match}\x1bC[0]`;
+    });
+  }
+}
+
+class HighlightWord {
+  constructor(word, color) {
+    this._word = word;
+    this._color = color;
+  }
+
+  get word() {
+    return this._word;
+  }
+
+  get color() {
+    return this._color;
+  }
+}
+
+const highlightWords = new HighlightWords();
+
+/**
+ * @param {Scene_Boot.prototype} sceneBoot
+ */
+function Scene_Boot_AutoHighlightMixIn(sceneBoot) {
+  const _start = sceneBoot.start;
+  sceneBoot.start = function() {
+    _start.call(this);
+    /**
+     * データベースのロードが完了しているので、ハイライトテキストを設定する。
+     */
+     settings.highlightGroups.forEach(highlightGroup => {
+      highlightGroup.texts.forEach(text => highlightWords.add(new HighlightWord(text, highlightGroup.color)));
+      highlightGroup.skills.forEach(skillId => highlightWords.add(new HighlightWord($dataSkills[skillId].name, highlightGroup.color)));
+      highlightGroup.items.forEach(itemId => highlightWords.add(new HighlightWord($dataItems[itemId].name, highlightGroup.color)));
+    });
+  };
+}
+
+Scene_Boot_AutoHighlightMixIn(Scene_Boot.prototype);
+
+const _Window_Base_convertEscapeCharacters = Window_Base.prototype.convertEscapeCharacters;
+Window_Base.prototype.convertEscapeCharacters = function (text) {
+  text = _Window_Base_convertEscapeCharacters.call(this, text);
+  /**
+   * WordWrapForJapanese の自動改行チェック時には処理しない
+   */
+  if (this.isHighlightWindow() && !this._checkWordWrapMode) {
+    return highlightWords.highlightText(text);
+  }
+  return text;
+};
+
+Window_Base.prototype.isHighlightWindow = function () {
+  return settings.targetWindows
+    .some(targetWindow => typeof window[targetWindow] === "function" &&
+      this instanceof window[targetWindow]);
+};
+
+PluginManager.isLoadedTorigoyaTextRuby = function () {
+  return !!window.Torigoya && !!window.Torigoya.TextRuby;
+};
+
+if (PluginManager.isLoadedTorigoyaTextRuby()) {
+  const rubyHighlightRegexp = highlightWords.getRegExpForRuby();
+  let TextRuby = window.Torigoya.TextRuby;
+
+  const _TextRuby_processDrawRuby = TextRuby.processDrawRuby;
+  TextRuby.processDrawRuby = function (mainText, subText, textState) {
+    if (rubyHighlightRegexp.test(mainText)) {
+      TextRuby.setMainTextColor(highlightWords.findColorByWord(mainText));
+    }
+    if (rubyHighlightRegexp.test(subText)) {
+      TextRuby.setSubTextColor(highlightWords.findColorByWord(subText));
+    }
+    _TextRuby_processDrawRuby.call(this, mainText, subText, textState);
+  };
+}
