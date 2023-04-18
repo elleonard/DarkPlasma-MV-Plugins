57228a28be2af8b5a428ba16532f72386605e5ed
plasma <6374339+elleonard@users.noreply.github.com>
2023-04-18 09:59:37 +0900

    1.0.1 不死身と組み合わせると敗北可を貫通する不具合を修正

--- src/codes/AllDeadState/DarkPlasma_AllDeadState.ts
+++ src/codes/AllDeadState/DarkPlasma_AllDeadState.ts
@@ -11,3 +11,22 @@ function Game_Party_AllDeadStateMixIn(gameParty: Game_Party) {
 }
 
 Game_Party_AllDeadStateMixIn(Game_Party.prototype);
+
+function Scene_Battle_ReviveMixIn(sceneBattle: Scene_Battle) {
+  const _terminate = sceneBattle.terminate;
+  sceneBattle.terminate = function () {
+    _terminate.call(this);
+    /**
+     * 不死身かつ、敗北判定ステートにより、HP0かつ戦闘不能でない状態で敗北した場合
+     * BattleManager.updateBattleEndの時点では戦闘不能でないため
+     * 戦闘不能の解除が働かず、マップ上に出た瞬間に敗北可であろうとゲームオーバーになってしまう
+     * この時点で全員戦闘不能かつ敗北可であった場合
+     * とりあえず全員復活させる
+     */
+    if (BattleManager.canLose() && $gameParty.isAllDead()) {
+      $gameParty.reviveBattleMembers();
+    }
+  };
+}
+
+Scene_Battle_ReviveMixIn(Scene_Battle.prototype);
