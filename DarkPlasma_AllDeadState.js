0400c838b7edea4ae2a9f11049d03d87b3dbf711
elleonard <librarian256@gmail.com>
2023-04-18 09:54:50 +0900

    57228a28be2af8b5a428ba16532f72386605e5ed

--- DarkPlasma_AllDeadState.js
+++ DarkPlasma_AllDeadState.js
@@ -1,9 +1,10 @@
-// DarkPlasma_AllDeadState 1.0.0
+// DarkPlasma_AllDeadState 1.0.1
 // Copyright (c) 2023 DarkPlasma
 // This software is released under the MIT license.
 // http://opensource.org/licenses/mit-license.php
 
 /**
+ * 2023/04/18 1.0.1 不死身と組み合わせると敗北可を貫通する不具合を修正
  * 2023/04/08 1.0.0 公開
  */
 
@@ -22,7 +23,7 @@
  * @default []
  *
  * @help
- * version: 1.0.0
+ * version: 1.0.1
  * アクターが指定したステートにかかっている場合、
  * 戦闘不能でなくとも敗北判定にカウントします。
  *
@@ -56,4 +57,21 @@
     };
   }
   Game_Party_AllDeadStateMixIn(Game_Party.prototype);
+  function Scene_Battle_ReviveMixIn(sceneBattle) {
+    const _terminate = sceneBattle.terminate;
+    sceneBattle.terminate = function () {
+      _terminate.call(this);
+      /**
+       * 不死身かつ、敗北判定ステートにより、HP0かつ戦闘不能でない状態で敗北した場合
+       * BattleManager.updateBattleEndの時点では戦闘不能でないため
+       * 戦闘不能の解除が働かず、マップ上に出た瞬間に敗北可であろうとゲームオーバーになってしまう
+       * この時点で全員戦闘不能かつ敗北可であった場合
+       * とりあえず全員復活させる
+       */
+      if (BattleManager.canLose() && $gameParty.isAllDead()) {
+        $gameParty.reviveBattleMembers();
+      }
+    };
+  }
+  Scene_Battle_ReviveMixIn(Scene_Battle.prototype);
 })();
